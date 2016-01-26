ecf7a79f63d82f627cfffb18bbed2b91a92aab3f
LewisTehMinerz <lewisminecraft@hotmail.co.uk>
2016-01-26 17:24:13 +0000

    Create sandbox.js

---
+++ javascripts/sandbox.js
@@ -0,0 +1,1 @@
+console.log("Hi! Welcome to the JSSandbox!\nPlease do not do anything stupid here.\n\nThere are preloaded libraries! These are:\njQuery\njQuery UI\nUnderscore.js\nMooTools\n\n-JSSandbox");
